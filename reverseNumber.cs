97d4a56b6bb2f2e2f59f5311b1db9d32cf4122de
TianyuanDeng <dty469783894@gmail.com>
2021-02-09 09:18:18 -0500

    ReverseNumber

---
+++ reverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmReview
+{
+    class reverseNumber
+    {
+        public static int ReverseNumber(int num)
+        {
+            int ans = 0;
+
+            while (num != 0)
+            {
+                int temp = num % 10;
+                num = num / 10;
+
+                if (ans > int.MaxValue / 10) return 0;
+                else if (ans < int.MinValue / 10) return 0;
+
+                ans = ans * 10 + temp;
+            }
+
+            return ans; 
+        }
+    }
+}
