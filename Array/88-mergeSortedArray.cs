7a1783fa53daa6439b6bbc6b5adfcf2ec2e3ce68
TianyuanDeng <dty469783894@gmail.com>
2024-06-03 16:56:26 -0700

    Add shoret solution

--- Array/88-mergeSortedArray.cs
+++ Array/88-mergeSortedArray.cs
@@ -40,5 +40,22 @@ namespace MergeSortedArray
                 }
             }
         }
+        public void BetterMerge(int[] nums1, int m, int[] nums2, int n) {
+            int pointer1 = m - 1;
+            int pointer2 = n - 2;
+            int mainPointer = m + n - 1;
+
+            while (pointer1 >= 0 && pointer2 >= 0){
+                if (nums1[pointer1] > nums2[pointer2]){
+                    nums1[mainPointer--] = nums1[pointer1--];
+                }else {
+                    nums1[mainPointer--] = nums2[pointer2--];
+                }
+            }
+
+            while (pointer2 >= 0){
+                nums1[mainPointer--] = nums2[pointer2--];
+            }
+        }
     }
 }
